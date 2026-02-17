f545256331ed0979e75a2465f4612656e37a5014
danish <danishmirzamirza001@gmail.com>
2026-02-17 23:11:10 +0530

    index.js updated and added something new

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start Server
-app.listen(PORT, () => {
+try {
+  app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+} catch (error) {
+  console.log("Sever is not running", error);
+  
+}
